b681d34ee6205d4cf1939b7eb25b79bd6db8ddb4
Stan Carver II <stan@a1webconsulting.com>
2026-02-06 13:38:42 -0600

    added HTML5 to Java Spring app

--- java-spring/src/main/java/com/howdyworld/java/HowdyController.java
+++ java-spring/src/main/java/com/howdyworld/java/HowdyController.java
@@ -7,8 +7,26 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HowdyController {
-    @GetMapping(value = "/", produces = MediaType.TEXT_PLAIN_VALUE)
+    @GetMapping(value = "/", produces = MediaType.TEXT_HTML_VALUE)
     public String howdy() {
-        return "Howdy, World!\n";
+        return """
+                <!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1" />
+                    <title>Howdy from Java Spring</title>
+                  </head>
+                  <body>
+                    <h1>Howdy, World!</h1>
+                  </body>
+                </html>
+                """;
     }
+
+    // FUTURE
+    // @GetMapping(value = "/healthz", produces = MediaType.TEXT_PLAIN_VALUE)
+    // public String healthz() {
+    // return "ok\n";
+    // }
 }
