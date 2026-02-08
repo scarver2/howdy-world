71863e31af5dd3d219883c258486ed64227a2996
Stan Carver II <stan@a1webconsulting.com>
2026-02-07 20:26:48 -0600

    implemented proper loading of vitest config

--- javascript-react/vite.config.js
+++ javascript-react/vite.config.js
@@ -1,5 +1,5 @@
 // javascript-react/vite.config.js
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => ({
