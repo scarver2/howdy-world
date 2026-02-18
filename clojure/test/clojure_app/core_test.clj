9db601a65b834426ee882d1679c85034c1025e5e
Stan Carver II <stan@a1webconsulting.com>
2026-02-18 07:52:12 -0600

    Clojure (#30)
    * added testing policy
    
    * added Clojure and missing endpoints to README
    
    * added Clojure and missing endpoints to README
    
    * added Clojure endpoint
    
    * added missing filepaths comments
    
    * added missing filepaths comments

---
+++ clojure/test/clojure_app/core_test.clj
@@ -0,0 +1,12 @@
+;; clojure/test/clojure_app/core_test.clj
+(ns clojure-app.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-app.core :refer :all]
+            [clojure.string :as str]))
+
+(deftest homepage-test
+  (testing "homepage returns correct structure"
+    (let [{:keys [status headers body]} (homepage)]
+      (is (= 200 status))
+      (is (= "text/html" (get headers "Content-Type")))
+      (is (str/includes? body "<h1>Howdy, World!</h1>")))))
