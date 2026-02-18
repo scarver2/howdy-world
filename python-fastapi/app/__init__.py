66e49fc01f0fe6fc85fffc2926eb0f28fe5d9cbd
Stan Carver II <stan@a1webconsulting.com>
2026-02-17 19:58:08 -0600

    added Python FastAPI endpoint

---
+++ python-fastapi/app/__init__.py
@@ -0,0 +1,1 @@
+# python-fastapi/app/__init__.py
---
+++ python-fastapi/app/main.py
@@ -0,0 +1,24 @@
+# python-fastapi/app/main.py
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+# from fastapi.staticfiles import StaticFiles
+
+app = FastAPI(title="Howdy World")
+
+HOWDY_HTML = """<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Howdy from Python FastAPI</title>
+</head>
+<body>
+  <h1>Howdy, World!</h1>
+</body>
+</html>
+"""
+
+@app.get("/", response_class=HTMLResponse)
+def root():
+    return HOWDY_HTML
+
+# app.mount("/", StaticFiles(directory="public", html=True), name="public")
