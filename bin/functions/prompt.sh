71c1552423d8b619d220054c0d7ffb89c77ecfc3
Stan Carver II <stan@a1webconsulting.com>
2026-03-02 09:54:40 -0600

    added confirm function to scripts

--- bin/functions/prompt.sh
+++ bin/functions/prompt.sh
@@ -1,6 +1,25 @@
 #!/usr/bin/env bash
 # bin/functions/prompt.sh
 
+confirm() {
+  local message="$1"
+
+  while true; do
+    read -rp "$message [y/N]: " response
+    case "$response" in
+      y|Y|yes|YES)
+        return 0
+        ;;
+      n|N|no|NO|"")
+        return 1
+        ;;
+      *)
+        echo "Please answer y or n."
+        ;;
+    esac
+  done
+}
+
 # --------------------------------------------------
 # Padding
 # FIXEME: does not appear to be used
