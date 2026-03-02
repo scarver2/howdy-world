3a897eddd5df0d5a39682db425f2c4f957172f0d
Stan Carver II <stan@a1webconsulting.com>
2026-03-02 14:46:35 -0600

    show endpoint in banner when executed within endpoint

--- bin/functions/banner.sh
+++ bin/functions/banner.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 # bin/functions/banner.sh
 
+detect_endpoint() {
+  if [[ "$PWD" != "$ROOT_DIR" ]] && [[ -f "$PWD/Dockerfile" ]]; then
+    basename "$PWD"
+  fi
+}
+
 COMMAND_NAME="$(basename "${BASH_SOURCE[2]}")"
+ENDPOINT_NAME="$(detect_endpoint)"
 
 banner() {
   [[ "$HW_QUIET" == "1" ]] && return
@@ -9,8 +16,10 @@ banner() {
   echo "Version: $HW_VERSION"
   echo "Root:    $ROOT_DIR"
   echo "Command: $COMMAND_NAME"
-
+  if [[ -n "$ENDPOINT_NAME" ]]; then
+    echo "Endpoint: $ENDPOINT_NAME"
+  fi
   # TODO: consider removing since it may just be noise.
-  echo "Compose: $COMPOSE_FILE"
+  # echo "Compose: $COMPOSE_FILE"
   echo
 }
