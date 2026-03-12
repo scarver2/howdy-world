2628f32b48adf1e4e4bafd71a11af723ab1aa212
Stan Carver II <stan@a1webconsulting.com>
2026-03-12 17:17:07 -0500

    added specs to ruby-on-rails endpoint

---
+++ ruby-on-rails/spec/rails_helper.rb
@@ -0,0 +1,13 @@
+# ruby-on-rails/spec/rails_helper.rb
+# frozen_string_literal: true
+
+require 'spec_helper'
+ENV['RAILS_ENV'] ||= 'test'
+require_relative '../config/environment'
+require 'rspec/rails'
+require 'rack/test'
+
+RSpec.configure do |config|
+  config.infer_spec_type_from_file_location!
+  config.filter_rails_from_backtrace!
+end
