c915bb0de0dd6f8925c81cdaa623b3fcab7a6458
Stan Carver II <stan@a1webconsulting.com>
2026-03-11 22:35:38 -0500

    add bin scripts to ruby-falcon

---
+++ ruby-falcon/bin/_lib.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# ruby-falcon/bin/_lib.sh
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+ENDPOINT_DIR="$(cd "$SCRIPT_DIR/.." && pwd)"
+
+if [[ -f "$ENDPOINT_DIR/../bin/_lib.sh" ]]; then
+  source "$ENDPOINT_DIR/../bin/_lib.sh"
+fi
+
+cd "$ENDPOINT_DIR"
